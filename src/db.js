9502ee841c99a8935b35988a3b220a3d8c47e93f
Marcokbc <mameira55@gmail.com>
2025-11-24 18:54:24 -0300

    debt: db in memory

--- src/db.js
+++ src/db.js
@@ -1,9 +1,6 @@
 const sqlite3 = require("sqlite3").verbose();
-const path = require("path");
 
-const DB_PATH = process.env.DB_PATH || path.join(__dirname, "..", "dev.db");
-
-const db = new sqlite3.Database(DB_PATH, (err) => {
+const db = new sqlite3.Database(":memory:", (err) => {
   if (err) {
     console.error("Erro ao abrir DB:", err);
     process.exit(1);
